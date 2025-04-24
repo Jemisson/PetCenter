6c9e14f7731ad394a0d8d9230e85f3b0212aca24
Jemisson <jemison321@gmail.com>
2025-04-24 15:33:22 -0400

    Adiciona o carrousel

---
+++ assets/javascripts/script.js
@@ -0,0 +1,35 @@
+// movimento para o navbar
+let lastScrollTop = 0;
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function () {
+  let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (currentScroll > lastScrollTop) {
+    header.style.top = "-100px"; // Esconde
+  } else {
+    header.style.top = "0";      // Mostra
+  }
+
+  lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+});
+
+// Carrossel
+const container = document.getElementById("container-cards");
+document.getElementById("btn-left").addEventListener("click", () => {
+  container.scrollBy({ left: -580, behavior: "smooth" });
+});
+document.getElementById("btn-right").addEventListener("click", () => {
+  container.scrollBy({ left: 580, behavior: "smooth" });
+});
+
+setInterval(() => {
+  const maxScrollLeft = container.scrollWidth - container.clientWidth;
+  if (container.scrollLeft >= maxScrollLeft) {
+    container.scrollTo({ left: 0, behavior: "smooth" });
+  } else {
+    container.scrollBy({ left: 580, behavior: "smooth" });
+  }
+}, 3000);
+
+
